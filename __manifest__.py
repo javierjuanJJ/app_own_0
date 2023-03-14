c966b29e76e1033e89987e0bb3f03963edb57ce2
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-14 12:44:17 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'App Own 0',
+    'version': '0.1',
+    'category': 'Sales/CRM',
+    'sequence': 15,
+    'summary': 'A proof Odoo module',
+    'description': "A proof Odoo module",
+    'website': '',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'css': [''],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
